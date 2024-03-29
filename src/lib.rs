758f4c033f5dfdab6fd34c63a0252b8e6075332f
Seán de Búrca <leftmostcat@gmail.com>
2024-03-29 14:25:44 -0700

    Minor fixups

--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,11 @@ pub use types::*;
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("failed to serialize structure as XML")]
-    Serialize {
-        #[from]
-        source: xml_struct::Error,
-    },
+    Serialize(#[from] xml_struct::Error),
 
     #[error("failed to deserialize structure from XML")]
-    Deserialize {
-        #[from]
-        source: quick_xml::DeError,
-    },
+    Deserialize(#[from] quick_xml::DeError),
 
     #[error("error manipulating XML data")]
-    Xml {
-        #[from]
-        source: quick_xml::Error,
-    },
+    Xml(#[from] quick_xml::Error),
 }
