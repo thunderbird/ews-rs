88ca8fb8bdb34676d82ca077505b169203681240
Andrew Devries <andrew@andrewdevries.net>
2025-11-11 10:29:43 -0600

    Add deserialize test for GetItemResponseMessage

--- ews/src/types/get_item.rs
+++ ews/src/types/get_item.rs
@@ -36,7 +36,14 @@ pub struct GetItemResponseMessage {
 
 #[cfg(test)]
 mod tests {
-    use crate::{test_utils::assert_serialized_content, BaseShape};
+
+    use time::{Date, OffsetDateTime, Time};
+
+    use crate::{
+        test_utils::{assert_deserialized_content, assert_serialized_content},
+        ArrayOfRecipients, BaseShape, Body, BodyType, DateTime, ItemId, Mailbox, Message,
+        MimeContent, RealItem, Recipient, ResponseClass, ResponseMessages, Sensitivity,
+    };
 
     use super::*;
 
@@ -57,5 +64,106 @@ mod tests {
         let expected = r#"<GetItem xmlns="http://schemas.microsoft.com/exchange/services/2006/messages"><ItemShape><t:BaseShape>Default</t:BaseShape><t:IncludeMimeContent>true</t:IncludeMimeContent></ItemShape><ItemIds><t:ItemId Id="AAAlAF" ChangeKey="CQAAAB"/></ItemIds></GetItem>"#;
 
         assert_serialized_content(&get_item, "GetItem", expected);
+    }
+
+    #[test]
+    fn test_deserialize_get_item_response_message() {
+        let content = r##"<GetItemResponse xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages"
+                             xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"
+                             xmlns="http://schemas.microsoft.com/exchange/services/2006/messages">
+              <m:ResponseMessages>
+                <m:GetItemResponseMessage ResponseClass="Success">
+                  <m:ResponseCode>NoError</m:ResponseCode>
+                  <m:Items>
+                    <t:Message>
+                      <t:MimeContent CharacterSet="UTF-8">UmVjZWl</t:MimeContent>
+                      <t:ItemId Id="AAAlAFVz" ChangeKey="CQAAAB" />
+                      <t:Subject />
+                      <t:Sensitivity>Normal</t:Sensitivity>
+                      <t:Body BodyType="HTML">
+                        <html dir="ltr">
+                          <head>
+                            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                              <meta content="MSHTML 6.00.3790.2759" name="GENERATOR">
+                                <style title="owaParaStyle">P { MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px } </style>
+                              </head>
+                          <body ocsi="x">
+                            <div dir="ltr">
+                              <font face="Tahoma" color="#000000" size="2"></font>&amp;nbsp;
+                            </div>
+                          </body>
+                        </html>
+                      </t:Body>
+                      <t:Size>881</t:Size>
+                      <t:DateTimeSent>2006-10-28T01:37:06Z</t:DateTimeSent>
+                      <t:DateTimeCreated>2006-10-28T01:37:06Z</t:DateTimeCreated>
+                      <t:ResponseObjects>
+                        <t:ReplyToItem />
+                        <t:ReplyAllToItem />
+                        <t:ForwardItem />
+                      </t:ResponseObjects>
+                      <t:HasAttachments>false</t:HasAttachments>
+                      <t:ToRecipients>
+                        <t:Mailbox>
+                          <t:Name>User1</t:Name>
+                          <t:EmailAddress>User1@example.com</t:EmailAddress>
+                          <t:RoutingType>SMTP</t:RoutingType>
+                        </t:Mailbox>
+                      </t:ToRecipients>
+                      <t:IsReadReceiptRequested>false</t:IsReadReceiptRequested>
+                      <t:IsDeliveryReceiptRequested>false</t:IsDeliveryReceiptRequested>
+                      <t:From>
+                        <t:Mailbox>
+                          <t:Name>User2</t:Name>
+                          <t:EmailAddress>User2@example.com</t:EmailAddress>
+                          <t:RoutingType>SMTP</t:RoutingType>
+                        </t:Mailbox>
+                      </t:From>
+                      <t:IsRead>false</t:IsRead>
+                    </t:Message>
+                  </m:Items>
+                </m:GetItemResponseMessage>
+              </m:ResponseMessages>
+            </GetItemResponse>"##;
+
+        let expected = GetItemResponse {
+            response_messages: ResponseMessages {
+                response_messages: vec![ResponseClass::Success(GetItemResponseMessage {
+                    items: Items {
+                        inner: vec![RealItem::Message(Message {
+                            mime_content: Some(MimeContent {
+                                character_set: Some("UTF-8".to_string()),
+                                content: "UmVjZWl".to_string(),
+                            }),
+                            item_id: Some(ItemId {
+                                id: "AAAlAFVz".to_string(),
+                                change_key: Some("CQAAAB".to_string()),
+                            }),
+                            subject: None,
+                            sensitivity: Some(Sensitivity::Normal),
+                            body: Some(Body {
+                                body_type: BodyType::HTML,
+                                is_truncated: Some(false),
+                                content: Some(
+                                    r##"<html dir="ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta content="MSHTML 6.00.3790.2759" name="GENERATOR"><style title="owaParaStyle">P { MARGIN-TOP: 1px; MARGIN-BOTTOM: 0px } </style></head><body ocsi="x"><div dir="ltr"><font face="Tahoma" color="#000000" size="2"></font>&amp;nbsp;</div>"##.to_string(),
+                                ),
+                            }),
+                            attachments: None,
+                            size: Some(881),
+                            date_time_sent: Some(DateTime(OffsetDateTime::new_utc(Date::from_calendar_date(2006, time::Month::October, 28).unwrap(), Time::from_hms(01, 37, 06).unwrap()))),
+                            date_time_created: Some(DateTime(OffsetDateTime::new_utc(Date::from_calendar_date(2006, time::Month::October, 28).unwrap(), Time::from_hms(01, 37, 06).unwrap()))),
+                            has_attachments: Some(false),
+                            to_recipients: Some(ArrayOfRecipients(vec![Recipient { mailbox: Mailbox { name: Some("User1".to_string()), email_address: Some("User1@example.com".to_string()), routing_type: Some("SMTP".to_string()), mailbox_type: None, item_id: None } }])),
+                            is_read_receipt_requested: Some(false),
+                            is_delivery_receipt_requested: Some(false),
+                            from: Some(Recipient { mailbox: Mailbox { name: Some("User2".to_string()), email_address: Some("User2@example.com".to_string()), routing_type: Some("SMTP".to_string()), mailbox_type: None, item_id: None } }),
+                            is_read: Some(false),
+                            ..Default::default()
+                        })],
+                    },
+                })],
+            },
+        };
+        assert_deserialized_content(content, expected);
     }
 }
